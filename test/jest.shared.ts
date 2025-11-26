4146052cfd4a3dc3979dce8525cbf15a810ef679
moraiskaua <moraiskaua03@gmail.com>
2025-11-26 16:58:16 -0300

    config: Add path aliases for identity and shared libraries, and expand e2e test matching pattern.

--- test/jest.shared.ts
+++ test/jest.shared.ts
@@ -4,6 +4,8 @@ export default {
     testEnvironment: 'node',
     moduleNameMapper: {
         '^@contentModule/(.*)$': '<rootDir>/src/module/content/$1',
+        '^@identityModule/(.*)$': '<rootDir>/src/module/identity/$1',
+        '^@sharedLibs/(.*)$': '<rootDir>/src/module/shared/$1',
         '^@sharedModule/(.*)$': '<rootDir>/src/module/shared/module/$1',
         '^@src/(.*)$': '<rootDir>/src/$1',
         '^@database/(.*)$': '<rootDir>/database/$1',
